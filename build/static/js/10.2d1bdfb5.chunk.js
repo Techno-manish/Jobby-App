1f7480a7a4f8f7f317a66e5ed65103336f7fce42
Manish Kumar <manishkumar12340@gmail.com>
2024-11-09 08:21:16 +0000

    build Ready

---
+++ build/static/js/10.2d1bdfb5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpjobby-app"]=this["webpackJsonpjobby-app"]||[]).push([[10],{75:function(t,a,p){"use strict";p.d(a,"a",(function(){return n}));var c=p(62);function n(t){return Object(c.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M13 3L16.293 6.293 9.293 13.293 10.707 14.707 17.707 7.707 21 11 21 3z"}},{tag:"path",attr:{d:"M19,19H5V5h7l-2-2H5C3.897,3,3,3.897,3,5v14c0,1.103,0.897,2,2,2h14c1.103,0,2-0.897,2-2v-5l-2-2V19z"}}]})(t)}}}]);
+//# sourceMappingURL=10.2d1bdfb5.chunk.js.map
--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
+import {lazy, Suspense} from 'react'
 import {Route, Switch, Redirect} from 'react-router-dom'
-
-import LoginForm from './components/LoginForm'
-import Home from './components/Home'
-import Jobs from './components/Jobs'
-import JobItemDetails from './components/JobItemDetails'
-import NotFound from './components/NotFound'
-import ProtectedRoute from './components/ProtectedRoute'
 
 import './App.css'
 
+import Loader from 'react-loader-spinner'
+import ProtectedRoute from './components/ProtectedRoute'
+
+const LoginForm = lazy(() => import('./components/LoginForm'))
+const Home = lazy(() => import('./components/Home'))
+const Jobs = lazy(() => import('./components/Jobs'))
+const JobItemDetails = lazy(() => import('./components/JobItemDetails'))
+const NotFound = lazy(() => import('./components/NotFound'))
+
 const App = () => (
   <>
-    <Switch>
-      <Route exact path="/login" component={LoginForm} />
-      <ProtectedRoute exact path="/" component={Home} />
-      <ProtectedRoute exact path="/jobs" component={Jobs} />
-      <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
-      <Route path="/not-found" component={NotFound} />
-      <Redirect to="/not-found" />
-    </Switch>
+    <Suspense
+      fallback={
+        <div className="loader-container" data-testid="loader">
+          <Loader type="ThreeDots" color="#ffffff" height="50" width="50" />
+        </div>
+      }
+    >
+      <Switch>
+        <Route exact path="/login" component={LoginForm} />
+        <ProtectedRoute exact path="/" component={Home} />
+        <ProtectedRoute exact path="/jobs" component={Jobs} />
+        <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect to="/not-found" />
+      </Switch>
+    </Suspense>
   </>
 )
 
